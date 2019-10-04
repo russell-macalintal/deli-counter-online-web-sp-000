8ed46d8039554fd3ba724766604d867f3033cd69
Russell Macalintal <russell.macalintal@gmail.com>
2019-10-04 02:24:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
 # Write your code here.
+def line(line_array)
+  if line_array.length < 1
+    puts "The line is currently empty."
+  else
+    list = []
+    line_array.each_with_index { |name, index|
+      list << "#{index + 1}. #{name}"
+    }
+    puts "The line is currently: " + list.join(" ")
+  end
+end
+
+def take_a_number(line_array, name)
+
+end
+
+def now_serving(line_array)
+  
+end
